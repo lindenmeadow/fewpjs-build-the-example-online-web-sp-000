9e128203ae44b4a1ed6168e7aad2974eb91e9cca
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-06 21:47:10 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,6 +4,46 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let colorHeart = {
+  "red" : "",
+  "" : "red"
+};
+
+const modal = document.querySelector('#modal')
+
+function hidesModalError() {
+  modal.classList.add("hidden")
+}
+
+function showModalError() {
+  modal.classList.remove("hidden")
+}
+
+hidesModalError()
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.addEventListener("click", function(event) {
+    const likeStatus = event.target.innerText
+    let heart = event.target
+    if (event.target.classList.contains('like-glyph')){
+
+
+    }
+    mimicServerCall()
+    .then( () => {
+      event.target.innerText = event.target.innerText == EMPTY_HEART ? FULL_HEART : EMPTY_HEART;
+      heart.style.color = colorHeart[heart.style.color]
+    })
+    .catch ( () => {
+      let errorMessage = document.createElement('p')
+      errorMessage.innerHTML = "You encountered a random server error.  Please try again."
+      modal.appendChild(errorMessage);
+      showModalError();
+      setTimeout(() => {hidesModalError(); modal.removeChild(errorMessage)}, 5000);
+    })
+  })
+});
 
 
 
